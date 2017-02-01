2655d2129263b101ea818441aebeeef4384c2ff1
karronoli <karo+git@karonori.com>
2017-02-01 12:41:36 +0900

    Assign start/stop character at indivisual parameter

--- TinySato/Barcode.cs
+++ TinySato/Barcode.cs
@@ -35,15 +35,23 @@ namespace TinySato
         protected static string CodabarDataCharacters { get; } = "0123456789-$:/.+";
         public static string CodabarSymbols { get; } = CodabarDataCharacters + CodabarStartStopCharacters;
 
+        public void AddCodabar13(int thin_bar_width, int bar_top_length, string print_data,
+            char start_stop_char = 'A')
+        {
+            AddCodabar13(thin_bar_width, bar_top_length, print_data,
+                start_stop_char, start_stop_char);
+        }
+
         /// <summary>
         /// Specifies a 1:3 ratio Codabar barcode with a narrow bar and wide bar.
         /// </summary>
         /// <param name="thin_bar_width">Thin Bar Width</param>
         /// <param name="bar_top_length">Bar Top Length</param>
         /// <param name="print_data">Print Data</param>
-        /// <param name="start_stop_char">Start / Stop Character</param>
+        /// <param name="start_char">Start Character</param>
+        /// <param name="stop_char">Stop Character</param>
         public void AddCodabar13(int thin_bar_width, int bar_top_length, string print_data,
-            char start_stop_char = 'A')
+            char start_char = 'A', char stop_char = 'A')
         {
             if (!(1 <= thin_bar_width && thin_bar_width <= 12))
                 throw new TinySatoException("Specify 1-12 dot for Narrow Bar Width.");
@@ -51,12 +59,14 @@ namespace TinySato
                 throw new TinySatoException("Specify 1-600 dot for Barcode Height.");
             if (!print_data.All(pd => CodabarDataCharacters.Contains(pd)))
                 throw new TinySatoException("Check character of barcode data.");
-            if (!CodabarStartStopCharacters.Contains(start_stop_char))
-                throw new TinySatoException("Check start/stop character.");
+            if (!CodabarStartStopCharacters.Contains(start_char))
+                throw new TinySatoException("Check start character.");
+            if (!CodabarStartStopCharacters.Contains(stop_char))
+                throw new TinySatoException("Check stop character.");
 
             const int barcode_type = 0; // 0: Codabar
             string print_data_with_control =
-                string.Format("{0}{1}{0}", start_stop_char, print_data);
+                string.Format("{0}{1}{2}", start_char, print_data, stop_char);
             this.printer.Add(string.Format("B{0:D1}{1:D2}{2:D3}{3}",
                 barcode_type, thin_bar_width, bar_top_length, print_data_with_control));
         }
