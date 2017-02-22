98c8d96e9f209e98238a705148c6c233377f0226
karronoli <karo+git@karonori.com>
2017-02-22 11:05:26 +0900

    Change codabar signature

--- TinySato/Barcode.cs
+++ TinySato/Barcode.cs
@@ -4,15 +4,15 @@ namespace TinySato
     using System.Collections.Generic;
     using System.Linq;
 
-    public enum CodabarType
-    {
-        Ratio12,
-        Ratio13,
-        Ratio25
-    }
-
     public class Barcode
     {
+        public enum Ratio
+        {
+            NARROW1_WIDE2,
+            NARROW1_WIDE3,
+            NARROW2_WIDE5
+        }
+
         protected Printer printer;
 
         internal Barcode(Printer printer)
@@ -42,34 +42,41 @@ namespace TinySato
             this.printer.Add(string.Format("BD3{0:D2}{1:D3}{2}", thin_bar_width, barcode_top, print_data));
         }
 
-        protected static Dictionary<CodabarType, string> CodabarOperands { get; }
-        = new Dictionary<CodabarType, string>()
+        protected static Dictionary<Ratio, string> RatioOperands { get; }
+        = new Dictionary<Ratio, string>()
         {
-            { CodabarType.Ratio12, "D" },
-            { CodabarType.Ratio13, "B" },
-            { CodabarType.Ratio25, "BD" }
+            { Ratio.NARROW1_WIDE2, "D" },
+            { Ratio.NARROW1_WIDE3, "B" },
+            { Ratio.NARROW2_WIDE5, "BD" }
         };
         protected static string CodabarStartStopCharacters { get; } = "ABCD";
         protected static string CodabarDataCharacters { get; } = "0123456789-$:/.+";
         public static string CodabarSymbols { get; } = CodabarDataCharacters + CodabarStartStopCharacters;
 
-        public void AddCodabar(CodabarType ct, int thin_bar_width, int bar_top_length, string print_data,
+        public void AddCodabar(int thin_bar_width, int bar_top_length, string print_data,
             char start_stop_char = 'A')
         {
-            AddCodabar(ct, thin_bar_width, bar_top_length, print_data,
+            AddCodabar(Ratio.NARROW1_WIDE3, thin_bar_width, bar_top_length, print_data,
+                start_stop_char, start_stop_char);
+        }
+
+        public void AddCodabar(Ratio r, int thin_bar_width, int bar_top_length, string print_data,
+            char start_stop_char = 'A')
+        {
+            AddCodabar(r, thin_bar_width, bar_top_length, print_data,
                 start_stop_char, start_stop_char);
         }
 
         /// <summary>
         /// Specify a 1:2 or 1:3 or 2:5 ratio Codabar barcode with a narrow bar and wide bar.
         /// </summary>
-        /// <param name="ct">Codabar Type</param>
+        /// <param name="r">Ratio</param>
         /// <param name="thin_bar_width">Thin Bar Width</param>
         /// <param name="bar_top_length">Bar Top Length</param>
         /// <param name="print_data">Print Data</param>
         /// <param name="start_char">Start Character</param>
         /// <param name="stop_char">Stop Character</param>
-        public void AddCodabar(CodabarType ct, int thin_bar_width, int bar_top_length, string print_data,
+        public void AddCodabar(Ratio r, int thin_bar_width, int bar_top_length, string print_data,
             char start_char, char stop_char)
         {
             if (!(1 <= thin_bar_width && thin_bar_width <= 12))
@@ -87,7 +94,7 @@ namespace TinySato
             string print_data_with_control =
                 string.Format("{0}{1}{2}", start_char, print_data, stop_char);
             this.printer.Add(string.Format("{0}{1:D1}{2:D2}{3:D3}{4}",
-                CodabarOperands[ct], barcode_type,
+                RatioOperands[r], barcode_type,
                 thin_bar_width, bar_top_length, print_data_with_control));
         }
     }
