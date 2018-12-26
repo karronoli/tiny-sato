626475009f079badd5096cc991388fef0cbcec83
karronoli <karo+git@karonori.com>
2018-12-26 18:28:38 +0900

    Rewrite unittest for no printer driver.
    I can't install "SATOV6 Advanced Port Monitor" on Terminal Service environment (Appveyor)

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -7,231 +7,227 @@ namespace UnitTestProject
     using System.IO;
     using System.Linq;
     using System.Net;
+    using System.Net.NetworkInformation;
     using System.Net.Sockets;
-    using System.Printing;
     using System.Text;
+    using System.Threading.Tasks;
     using TinySato;
+    using TinySato.Search;
 
     [TestClass]
     public class UnitTest1
     {
-        protected const string printer_name = "T408v";
-        protected const string printer_ip = "127.0.0.1";
-        protected const int printer_port = 9100;
-        protected const byte STX = 0x02, ETX = 0x03, ESC = 0x1b;
-        protected static TcpListener listener;
+        const string printer_mac = "02:00:00:00:00:01";
+        static readonly string printer_mac_eui48 = printer_mac.Replace(':', '-').ToUpper();
+
+        const byte SOH = 0x01, STX = 0x02, ETX = 0x03, ENQ = 0x05, ESC = 0x1b;
+        const byte ASCII_A = 0x41, ASCII_L = 0x4c, ASCII_Z = 0x5a;
+
+        static readonly byte[] SearchResponseBody = File.ReadAllBytes("search-response.bin");
+        static readonly byte[] HealthOKBody = File.ReadAllBytes("health-ok.bin");
+
+        static readonly IPEndPoint searchEP = new IPEndPoint(IPAddress.Any, 19541);
+        static readonly IPEndPoint printEP = new IPEndPoint(IPAddress.Any, 9100);
+        static TcpListener listener;
 
         const double inch2mm = 25.4;
         const double dpi = 203;
         const double dot2mm = inch2mm / dpi; // 25.4(mm) / 203(dot) -> 0.125(mm/dot)
         const double mm2dot = 1 / dot2mm; // 8(dot/mm)
 
-        [ClassInitialize]
-        public static void Listen(TestContext context)
+        [TestInitialize]
+        public void Listen()
         {
-            listener = new TcpListener(IPAddress.Parse(printer_ip), printer_port)
-            {
-                ExclusiveAddressUse = true
-            };
-            listener.Start();
+            listener = new TcpListener(printEP) { ExclusiveAddressUse = true };
+            listener.Start(1);
         }
 
-        [ClassCleanup]
-        public static void Stop()
+        [TestCleanup]
+        public void Stop()
         {
             listener.Stop();
         }
 
-        protected static byte[] GetBinary()
+        protected async static Task<byte[]> ResponseForPrint()
         {
-            var client = listener.AcceptTcpClient();
-            var buffer = new List<byte[]>();
+            var buffers = new List<byte[]>();
+
+            using (var client = await listener.AcceptTcpClientAsync())
             using (var stream = client.GetStream())
-            {
-                var raw = new byte[1024];
-                int ret;
-                while ((ret = stream.Read(raw, 0, raw.Length)) != 0)
-                {
-                    var dest = new byte[ret];
-                    Array.Copy(raw, dest, ret);
-                    buffer.Add(dest);
-                }
-            }
-            client.Close();
-            return buffer.SelectMany(x => x).ToArray();
-        }
-
-        protected static int GetJobCount()
-        {
-            using (var server = new LocalPrintServer())
-            using (var queue = server.GetPrintQueue(printer_name))
-            {
-                return queue.NumberOfJobs;
-            }
-        }
-
-        protected static int GetLastJobPageCount()
-        {
-            using (var server = new LocalPrintServer())
-            using (var queue = server.GetPrintQueue(printer_name))
-            using (var jobs = queue.GetPrintJobInfoCollection())
-            {
-                var last = jobs.OrderBy(job => job.TimeJobSubmitted).Last();
-                return last.NumberOfPages;
-            }
-        }
-
-        [TestCleanup]
-        public void TearDown()
-        {
-            using (var server = new LocalPrintServer())
-            using (var queue = server.GetPrintQueue(printer_name))
             {
                 while (true)
                 {
-                    using (var jobs = queue.GetPrintJobInfoCollection())
-                    {
-                        if (jobs.Count() == 0)
-                        {
-                            break;
-                        }
+                    var dummy = new byte[client.ReceiveBufferSize];
+                    var actual_buffer_length = await stream.ReadAsync(dummy, 0, dummy.Length);
+                    var buffer = dummy.Take(actual_buffer_length);
 
-                        foreach (var job in jobs)
-                        {
-                            if (job.IsPrinting)
-                            {
-                                job.Cancel();
-                            }
-                        }
+                    if (buffer.Count() == 0)
+                    {
+                        var last = buffers.Last();
+                        if (last.Last() == ETX) break;
                     }
+
+                    if (buffer.Last() == ENQ)
+                        await stream.WriteAsync(HealthOKBody, 0, HealthOKBody.Length);
+
+                    buffers.Add(buffer.ToArray());
                 }
+
+                return buffers.SelectMany(buffer => buffer.Where(b => b != ENQ)).ToArray();
             }
         }
 
-        [TestMethod]
-        public void MultiJob()
+        protected async static Task<byte[]> ResponseForSearch()
         {
-            var before = GetJobCount();
-            var now = DateTime.Now;
-            var first = new Printer(printer_name);
-            var second = new Printer(printer_name);
-            Assert.AreEqual(before + 2, GetJobCount());
-
-            // send before first job, but block until first job ending.
-            second.Send(); // send <A><Z>, job +1
-            first.SetCalendar(now); // To add job, need a operation at least.
-            first.Close();
-            second.Close();
-
-            var actual1 = GetBinary();
-            var expected1 = new string[]
+            UdpReceiveResult result;
+            using (var server = new UdpClient(searchEP) { EnableBroadcast = true })
             {
-                "A",
-                string.Format("WT{0:D2}{1:D2}{2:D2}{3:D2}{4:D2}",
-                    now.Year % 1000, now.Month, now.Day, now.Hour, now.Minute),
-                "Z",
-            }.SelectMany(x => (new byte[] { ESC }).Concat(Encoding.ASCII.GetBytes(x))).ToList();
-            expected1.Insert(0, STX);
-            expected1.Add(ETX);
+                server.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                result = await server.ReceiveAsync();
+            }
 
-            var actual2 = GetBinary();
-            var expected2 = new string[]
+            await Task.Delay(TimeSpan.FromMilliseconds(10));
+
+            using (var client = new UdpClient() { EnableBroadcast = true })
             {
-                "A",
-                "Z",
-            }.SelectMany(x => (new byte[] { ESC }).Concat(Encoding.ASCII.GetBytes(x))).ToList();
-            expected2.Insert(0, STX);
-            expected2.Add(ETX);
+                client.Connect(new IPEndPoint(IPAddress.Broadcast, result.RemoteEndPoint.Port));
+                await client.SendAsync(SearchResponseBody, SearchResponseBody.Length);
+            }
 
-            CollectionAssert.AreEqual(
-                expected1.Concat(expected2).ToArray(),
-                actual1.Concat(actual2).ToArray());
+            return result.Buffer;
         }
 
         [TestMethod]
-        public void MultiLabelAtSingleJob()
+        public async Task SearchPrinter()
         {
-            var before = GetJobCount();
-            using (var printer = new Printer(printer_name))
+            var task = ResponseForSearch();
+            var wait_time = TimeSpan.FromMilliseconds(500);
+
+            Response response = null;
+            using (task)
+            {
+                var mac = PhysicalAddress.Parse(printer_mac_eui48);
+                Printer.ClearSearchCache();
+                var responses = Printer.Search(wait_time).Where(r => r.MACAddress.Equals(mac));
+                Assert.IsInstanceOfType(responses, typeof(IEnumerable<Response>));
+                CollectionAssert.AreEqual(new byte[] { SOH, ASCII_L, ASCII_A }, await task);
+                Assert.AreEqual(1, responses.Count());
+                response = responses.First();
+            }
+
+            Assert.AreEqual(IPAddress.Parse("127.0.0.1"), response.IPAddress);
+            Assert.AreEqual(IPAddress.Parse("255.0.0.0"), response.SubnetMask);
+            Assert.AreEqual(IPAddress.Parse("0.0.0.0"), response.Gateway);
+            Assert.AreEqual("Lesprit Series", response.Name);
+            Assert.IsTrue(response.DHCP);
+            Assert.IsTrue(response.RARP);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(TinySatoException))]
+        public void BusyPrinter()
+        {
+            listener.Stop();
+            var task = ResponseForSearch();
+            Printer.ClearSearchCache();
+            using (task)
+            using (var printer = Printer.Find(printer_mac)) { }
+        }
+
+        [TestMethod]
+        public async Task MultiLabel()
+        {
+            var task = ResponseForSearch();
+            var task2 = ResponseForPrint();
+
+            Printer.ClearSearchCache();
+
+            using (task)
+            using (var printer = Printer.Find(printer_mac))
             {
                 // page 1
                 printer.Barcode.AddCODE128(1, 2, "HELLO");
                 printer.SetPageNumber(3);
                 printer.AddStream();
+
                 // page 2
                 printer.Barcode.AddCODE128(4, 5, "WORLD");
                 printer.SetPageNumber(6);
                 printer.AddStream();
+
                 // page 3
                 printer.Barcode.AddCODE128(7, 8, "!!!");
-                printer.Send(9);
-                // page 4 (empty page)
-                printer.Send(1);
+                printer.SetPageNumber(9);
+                printer.Send();
             }
-            Assert.AreEqual(before + 1, GetJobCount(), "Job count");
-            Assert.AreEqual(4, GetLastJobPageCount(), "Variation of page");
 
             var expected = new string[]
             {
+                // page 1
                 "A",
                 "BG" + "01" + "002" + "HELLO",
                 "Q" + "000003",
                 "Z",
+
+                // page 2
                 "A",
                 "BG" + "04" + "005" + "WORLD",
                 "Q" + "000006",
                 "Z",
+
+                // page 3
                 "A",
                 "BG" + "07" + "008" + "!!!",
                 "Q" + "000009",
                 "Z",
-            }.SelectMany(x => (new byte[] { ESC }).Concat(Encoding.ASCII.GetBytes(x))).ToList();
-            expected.Insert(0, STX);
-            expected.Add(ETX);
+            }.SelectMany(x => (new byte[] { ESC }).Concat(Encoding.ASCII.GetBytes(x)))
+            .Prepend(STX).Append(ETX);
 
-            // empty page
-            var expected_empty = new string[]
+            using (task2)
             {
-                "A",
-                "Q" + "000001",
-                "Z",
-            }.SelectMany(x => (new byte[] { ESC }).Concat(Encoding.ASCII.GetBytes(x))).ToList();
-            expected_empty.Insert(0, STX);
-            expected_empty.Add(ETX);
-            var actual = GetBinary();
-            CollectionAssert.AreEqual(expected.Concat(expected_empty).ToArray(), actual);
+                var actual = await task2;
+                CollectionAssert.AreEqual(expected.ToArray(), actual);
+            }
         }
 
         [TestMethod]
-        public void ExampleBarcode()
+        public async Task ExampleBarcode()
         {
-            var before = GetJobCount();
             var barcode = "1234567890128";
+            var task = ResponseForSearch();
+            var task2 = ResponseForPrint();
+            Printer.ClearSearchCache();
 
-            using (var sato = new Printer(printer_name))
+            using (task)
+            using (var printer = Printer.Find(printer_mac))
             {
-                sato.SetSensorType(SensorType.Reflection);
-                sato.SetGapSizeBetweenLabels((int)Math.Round(2.0 * mm2dot));
-                sato.SetDensity(3, DensitySpec.A);
-                sato.SetSpeed(4);
-                sato.SetPaperSize((int)(80 * mm2dot), (int)(104 * mm2dot));
-                sato.SetStartPosition(0, 0);
+                Assert.IsInstanceOfType(printer, typeof(Printer));
+                Assert.AreEqual(ConnectionType.IP, printer.ConnectionType);
 
-                sato.MoveToX(80);
-                sato.MoveToY(80);
-                sato.Barcode.AddJAN13(3, 70, barcode);
-                sato.MoveToX(8);
-                sato.MoveToY(8);
-                sato.Barcode.AddCodabar(1, 2, barcode);
+                // settings
+                printer.SetSensorType(SensorType.Reflection);
+                printer.SetGapSizeBetweenLabels((int)Math.Round(2.0 * mm2dot));
+                printer.SetDensity(3, DensitySpec.A);
+                printer.SetSpeed(4);
+                printer.SetPaperSize((int)(80 * mm2dot), (int)(104 * mm2dot));
+                printer.SetStartPosition(0, 0);
 
-                sato.SetPageNumber(1);
-                sato.Send();
+                // drawings
+                printer.MoveToX(80);
+                printer.MoveToY(80);
+                printer.Barcode.AddJAN13(3, 70, barcode);
+                printer.MoveToX(8);
+                printer.MoveToY(8);
+                printer.Barcode.AddCodabar(1, 2, barcode);
+
+                // print
+                printer.SetPageNumber(1);
+                printer.Send();
             }
 
-            var after = GetJobCount();
-            Assert.AreEqual(before + 1, after);
-
-            var expected = new string[] {
+            IEnumerable<byte> expected = new string[]
+            {
+                // settings
                 "A",
                 "IG" + "0",
                 "Z",
@@ -249,33 +245,45 @@ namespace UnitTestProject
                 "Z",
                 "A",
                 "A3V+000H+000",
+
+                // drawings
                 "H" + "0080",
                 "V" + "0080",
-                "BD" + "3" + "03" + "070" + "1234567890128",
+                "BD" + "3" + "03" + "070" + barcode,
                 "H" + "0008",
                 "V" + "0008",
-                "B" + "0" + "01" + "002" + "A1234567890128A",
+                "B" + "0" + "01" + "002" + "A" + barcode + "A",
+
+                // print
                 "Q" + "000001",
                 "Z"
-            }.SelectMany(x => (new byte[] { ESC }).Concat(Encoding.ASCII.GetBytes(x))).ToList();
-            expected.Insert(0, STX);
-            expected.Add(ETX);
-            var actual = GetBinary();
-            CollectionAssert.AreEqual(expected.ToArray(), actual);
+            }.SelectMany(x => (new byte[] { ESC }).Concat(Encoding.ASCII.GetBytes(x)))
+            .Prepend(STX).Append(ETX);
+
+            using (task2)
+            {
+                var actual = await task2;
+                CollectionAssert.AreEqual(expected.ToArray(), actual);
+            }
         }
 
         [TestMethod]
-        public void ExampleGraphic()
+        public async Task ExampleGraphic()
         {
-            var before = GetJobCount();
-            using (var sato = new Printer(printer_name))
+            var task = ResponseForSearch();
+            var task2 = ResponseForPrint();
+            Printer.ClearSearchCache();
+
+            using (task)
+            using (var printer = Printer.Find(printer_mac))
             {
                 int width = (int)Math.Round(85.0 * mm2dot),
                     height = (int)Math.Round(50.0 * mm2dot);
 
-                sato.MoveToX((int)Math.Round(1.3 * mm2dot));
-                sato.MoveToY(1);
-                sato.Graphic.AddBox(
+                // box
+                printer.MoveToX((int)Math.Round(1.3 * mm2dot));
+                printer.MoveToY(1);
+                printer.Graphic.AddBox(
                     (int)Math.Round(0.5 * mm2dot),
                     (int)Math.Round(0.5 * mm2dot),
                     width,
@@ -283,37 +291,51 @@ namespace UnitTestProject
 
                 using (var bitmap = (Bitmap)Image.FromFile("input.png"))
                 {
-                    sato.MoveToX((int)Math.Round(1.3 * mm2dot));
-                    sato.MoveToY(1);
-                    sato.Graphic.AddGraphic(bitmap);
-                    sato.MoveToX(1);
-                    sato.MoveToY(1);
-                    sato.Graphic.AddBitmap(bitmap);
-                }
-                sato.SetPageNumber(1);
-                sato.Send();
-            }
-            var after = GetJobCount();
-            Assert.AreEqual(before + 1, after, "Job count");
+                    // graphic
+                    printer.MoveToX((int)Math.Round(1.3 * mm2dot));
+                    printer.MoveToY(1);
+                    printer.Graphic.AddGraphic(bitmap);
 
-            var _expected = new string[] {
+                    // bitmap
+                    printer.MoveToX(1);
+                    printer.MoveToY(1);
+                    printer.Graphic.AddBitmap(bitmap);
+                }
+                // print
+                printer.SetPageNumber(1);
+                printer.Send();
+            }
+
+            IEnumerable<byte> expected = new string[]
+            {
                 "A",
+
+                // box
                 "H0010",
                 "V0001",
                 "FW0404V0400H0679",
+
+                // graphic
                 "H0010",
                 "V0001",
                 "GH108067" + File.ReadAllText("output.txt"),
+
+                // bitmap
                 "H0001",
                 "V0001",
                 string.Format("GM{0},", new FileInfo("output.bmp").Length),
-            }.SelectMany(x => (new byte[] { ESC }).Concat(Encoding.ASCII.GetBytes(x)));
-            _expected = _expected.Concat(File.ReadAllBytes("output.bmp"));
-            _expected = _expected.Concat(new byte[] { ESC }.Concat(Encoding.ASCII.GetBytes("Q000001")));
+            }.SelectMany(x => (new byte[] { ESC }).Concat(Encoding.ASCII.GetBytes(x)))
+            .Concat(File.ReadAllBytes("output.bmp"))
+            // print
+            .Append(ESC).Concat(Encoding.ASCII.GetBytes("Q000001"))
+            .Concat(new byte[] { ESC, ASCII_Z })
+            .Prepend(STX).Append(ETX);
 
-            var expected = new byte[] { STX }.Concat(_expected).Concat(new byte[] { ESC, Convert.ToByte('Z'), ETX });
-            var actual = GetBinary();
-            CollectionAssert.AreEqual(expected.ToArray(), actual);
+            using (task2)
+            {
+                var actual = await task2;
+                CollectionAssert.AreEqual(expected.ToArray(), actual);
+            }
         }
     }
 }
