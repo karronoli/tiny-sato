a3fd9e6818a3417ccd810c903b58d1da4d411718
karronoli <karo+git@karonori.com>
2018-12-26 18:28:33 +0900

    Print by IP without printer driver.
    ## New
    
    1. `new Printer(IPEndpoint)`
    2. Check printer status by `new StatusInformation(Stream)`
    3. `Send()` -> `Send(byte[])` -> `SendTcp(byte[])` if status is OK
    
    ## Ordinary
    
    1. `new Printer(string)`
    2. `Send()` -> `Send(byte[])` -> `SendWin32(byte[])`

---
+++ TinySato/Communication/JobStatus.cs
@@ -0,0 +1,228 @@
+﻿namespace TinySato.Communication
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.InteropServices;
+
+    public class JobStatus
+    {
+        public string ID { get; }
+
+        public Health Health { get; }
+
+        public int LabelRemaining { get; }
+
+        public string Name { get; }
+
+        static readonly byte[] RequestBody = new byte[] { Printer.ASCII_ENQ };
+
+        static readonly TimeSpan IOWaitTimeout = TimeSpan.FromSeconds(10.0);
+
+        // Don't dispose for persistent connection to printer
+        protected Stream stream;
+
+        public JobStatus(Stream stream)
+        {
+            this.stream = stream;
+
+            RawJobStatus response;
+            var writeTimeoutOrginal = stream.WriteTimeout;
+            var readTimeoutOriginal = stream.ReadTimeout;
+
+            try
+            {
+                // Enquire status
+                stream.WriteTimeout = (int)IOWaitTimeout.TotalMilliseconds;
+                stream.Write(RequestBody, 0, RequestBody.Length);
+                // Return status
+                var buffer = new byte[1024];
+                stream.ReadTimeout = (int)IOWaitTimeout.TotalMilliseconds;
+                var actual_length = stream.Read(buffer, 0, buffer.Length);
+                // Marshal status
+                var expected_length = Marshal.SizeOf<RawJobStatus>();
+                var ptr = Marshal.AllocCoTaskMem(expected_length);
+                // Skip Ethernet padding
+                Marshal.Copy(buffer, actual_length - expected_length, ptr, expected_length);
+                response = Marshal.PtrToStructure<RawJobStatus>(ptr);
+                Marshal.FreeCoTaskMem(ptr);
+            }
+            catch (IOException e)
+            {
+                throw new TinySatoException("The printer is not respond.", e);
+            }
+            finally
+            {
+                stream.WriteTimeout = writeTimeoutOrginal;
+                stream.ReadTimeout = readTimeoutOriginal;
+            }
+
+            if (response.STX != Printer.ASCII_STX || response.ETX != Printer.ASCII_ETX)
+            {
+                throw new NotImplementedException();
+            }
+
+            ID = response.ID;
+            Health = new Health(response.Health);
+            LabelRemaining = int.Parse(response.LabelRemaining);
+            Name = response.Name;
+
+            if (Health.Error != Error.None)
+                throw new TinySatoException($"Printer failure. error: {Enum.GetName(typeof(Error), Health.Error)}");
+        }
+
+        public bool OK
+        {
+            get
+            {
+                return (Health.State == State.Online
+                    || Health.State == State.OnlinePrinting
+                    || Health.State == State.OnlineDispense
+                    || Health.State == State.OnlineAnalyzing)
+                    && Health.Battery == Battery.OK
+                    && Health.Buffer == Buffer.OK;
+            }
+        }
+
+        public JobStatus Refresh() => new JobStatus(stream);
+
+        public override string ToString()
+        {
+            var message = $"{nameof(ID)}: {ID}";
+            message += $", {nameof(Name)}: {Name}";
+            message += $", {nameof(LabelRemaining)}: {LabelRemaining}";
+            message += $", {nameof(Health.State)}: {Enum.GetName(typeof(State), Health.State)}";
+            message += $", {nameof(Health.Battery)}: {Enum.GetName(typeof(Battery), Health.Battery)}";
+            message += $", {nameof(Health.Buffer)}: {Enum.GetName(typeof(Buffer), Health.Buffer)}";
+            message += $", {nameof(Health.Error)}: {Enum.GetName(typeof(Error), Health.Error)}";
+            return message;
+        }
+    }
+
+    public enum State
+    {
+        Offline,
+        Online, OnlinePrinting, OnlineDispense, OnlineAnalyzing,
+        Error
+    }
+
+    public enum Battery { NearEnd, OK, Unknown }
+
+    public enum Buffer { NearFull, OK, Unknown }
+
+    public enum Error { None, Buffer, Paper, Battery, Sensor, Head, CoverOpen, Other }
+
+    public struct Health
+    {
+        public char Raw { get; }
+        public State State { get; }
+        public Battery Battery { get; }
+        public Buffer Buffer { get; }
+        public Error Error { get; }
+
+        static readonly List<Health> Definition = new List<Health>()
+        {
+            new Health('0', State.Offline),
+            new Health('1', State.Offline, Battery.NearEnd),
+            new Health('2', State.Offline, Buffer.NearFull),
+            new Health('3', State.Offline, Battery.NearEnd, Buffer.NearFull),
+
+            new Health('A', State.Online),
+            new Health('B', State.Online, Battery.NearEnd),
+            new Health('C', State.Online, Buffer.NearFull),
+            new Health('D', State.Online, Battery.NearEnd, Buffer.NearFull),
+
+            new Health('G', State.OnlinePrinting),
+            new Health('H', State.OnlinePrinting, Battery.NearEnd),
+            new Health('I', State.OnlinePrinting, Buffer.NearFull),
+            new Health('J', State.OnlinePrinting, Battery.NearEnd, Buffer.NearFull),
+
+            new Health('M', State.OnlineDispense),
+            new Health('N', State.OnlineDispense, Battery.NearEnd),
+            new Health('O', State.OnlineDispense, Buffer.NearFull),
+            new Health('P', State.OnlineDispense, Battery.NearEnd, Buffer.NearFull),
+
+            new Health('S', State.OnlineAnalyzing),
+            new Health('T', State.OnlineAnalyzing, Battery.NearEnd),
+            new Health('U', State.OnlineAnalyzing, Buffer.NearFull),
+            new Health('V', State.OnlineAnalyzing, Battery.NearEnd, Buffer.NearFull),
+
+            new Health('a', Error.Buffer),
+            new Health('c', Error.Paper),
+            new Health('d', Error.Battery),
+            new Health('f', Error.Sensor),
+            new Health('g', Error.Head),
+            new Health('h', Error.CoverOpen),
+            new Health('k', Error.Other),
+        };
+
+        public Health(char status)
+        {
+            var health = Definition.Where(h => h.Raw == status);
+            if (health.Count() != 1)
+                throw new TinySatoException($"Printer status is unknown. status:{status}");
+            this = health.First();
+        }
+
+        private Health(char raw, State state) : this(raw, state, Battery.OK, Buffer.OK) { }
+        private Health(char raw, State state, Battery battery) : this(raw, state, battery, Buffer.OK) { }
+        private Health(char raw, State state, Buffer buffer) : this(raw, state, Battery.OK, buffer) { }
+
+        private Health(char raw, State state, Battery battery, Buffer buffer)
+        {
+            Raw = raw;
+            State = state;
+            Battery = battery;
+            Buffer = buffer;
+            Error = Error.None;
+        }
+
+        private Health(char raw, Error error)
+        {
+            Raw = raw;
+            State = State.Error;
+            Battery = Battery.Unknown;
+            Buffer = Buffer.Unknown;
+            Error = error;
+        }
+
+        public override int GetHashCode() => Raw;
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Health))
+                return false;
+
+            return Equals((Health)obj);
+        }
+
+        public bool Equals(Health other) => Raw == other.Raw;
+
+        public static bool operator ==(Health left, Health right) => left.Equals(right);
+
+        public static bool operator !=(Health left, Health right) => !left.Equals(right);
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct RawJobStatus
+    {
+        [MarshalAs(UnmanagedType.U1)]
+        internal byte STX;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 2)]
+        internal string ID;
+
+        [MarshalAs(UnmanagedType.U1)]
+        internal char Health;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 6)]
+        internal string LabelRemaining;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 16)]
+        internal string Name;
+
+        [MarshalAs(UnmanagedType.U1)]
+        internal byte ETX;
+    }
+}
--- TinySato/Printer.cs
+++ TinySato/Printer.cs
@@ -1,26 +1,39 @@
 ﻿namespace TinySato
 {
+    using Communication;
     using System;
     using System.Collections.Generic;
     using System.ComponentModel;
+    using System.Diagnostics;
     using System.Linq;
+    using System.Net;
+    using System.Net.Sockets;
     using System.Runtime.InteropServices;
     using System.Text;
+    using System.Threading.Tasks;
 
     public partial class Printer : IDisposable
     {
         private bool disposed = false;
+
         protected bool send_at_dispose_if_not_yet_sent = false;
-        protected int operation_start_index = 1;
         private IntPtr printer = IntPtr.Zero;
+
+        static readonly TimeSpan ConnectWaitTimeout = TimeSpan.FromSeconds(3);
+        static readonly TimeSpan PrintSendTimeout = TimeSpan.FromMilliseconds(200); // CT408i driver default setting
+        private TcpClient client;
+
+        protected int operation_start_index = 1;
         protected List<byte[]> operations = new List<byte[]> {
             new byte[] { Convert.ToByte(STX) }
         };
+
         protected int soft_offset_x = 0;
         protected int soft_offset_y = 0;
 
         public Barcode Barcode { get; }
         public Graphic Graphic { get; }
+        public ConnectionType ConnectionType { get; }
 
         internal const char
             SOH = '\x01', STX = '\x02', ETX = '\x03', ENQ = '\x05', ESC = '\x1b';
@@ -33,13 +46,14 @@
             OPERATION_A = ESC + "A",
             OPERATION_Z = ESC + "Z";
 
-        public Printer(string PrinterName, bool send_at_dispose_if_not_yet_sent) : this(PrinterName)
-        {
-            this.send_at_dispose_if_not_yet_sent = send_at_dispose_if_not_yet_sent;
-        }
+        protected JobStatus status;
 
         public Printer(string name)
         {
+            this.ConnectionType = ConnectionType.Driver;
+            this.Barcode = new Barcode(this);
+            this.Graphic = new Graphic(this);
+
             if (!UnsafeNativeMethods.OpenPrinter(name.Normalize(), out printer, IntPtr.Zero))
                 throw new TinySatoException("failed to use printer.",
                     new Win32Exception(Marshal.GetLastWin32Error()));
@@ -47,8 +61,37 @@
             var di = new DOCINFO() { pDataType = "raw", pDocName = "RAW DOCUMENT" };
             if (!UnsafeNativeMethods.StartDocPrinter(printer, level, di))
                 throw new Win32Exception(Marshal.GetLastWin32Error());
+        }
+
+        public Printer(IPEndPoint endpoint)
+        {
+            this.ConnectionType = ConnectionType.IP;
             this.Barcode = new Barcode(this);
             this.Graphic = new Graphic(this);
+
+            this.client = new TcpClient() { SendTimeout = PrintSendTimeout.Milliseconds };
+            try
+            {
+                this.client.Connect(endpoint);
+            }
+            catch (SocketException e)
+            {
+                throw new TinySatoException($"The printer is maybe none in the same network. endpoint: {endpoint}", e);
+            }
+
+            this.status = new JobStatus(client.GetStream());
+            if (!status.OK)
+            {
+                var timer = Stopwatch.StartNew();
+                while (ConnectWaitTimeout > timer.Elapsed)
+                {
+                    Task.Delay(TimeSpan.FromMilliseconds(100)).Wait();
+                    this.status = status.Refresh();
+                    if (status.OK) break;
+                }
+                if (!status.OK)
+                    throw new TinySatoException($"Printer is busy. endpoint: {endpoint}, status: {status}");
+            }
         }
 
         public void MoveToX(int x)
@@ -161,25 +204,10 @@
             operation_start_index = operations.Count;
 
             var flatten = operations.SelectMany(x => x).ToArray();
-            var raw = Marshal.AllocCoTaskMem(flatten.Length);
-            Marshal.Copy(flatten, 0, raw, flatten.Length);
-            int written = 0;
-            try
-            {
-                if (!UnsafeNativeMethods.StartPagePrinter(printer))
-                    throw new Win32Exception(Marshal.GetLastWin32Error());
-                if (!UnsafeNativeMethods.WritePrinter(printer, raw, flatten.Length, out written))
-                    throw new Win32Exception(Marshal.GetLastWin32Error());
-                if (!UnsafeNativeMethods.EndPagePrinter(printer))
-                    throw new Win32Exception(Marshal.GetLastWin32Error());
-                this.operations.Clear();
-                operation_start_index = 0;
-            }
-            catch (Win32Exception inner)
-            {
-                throw new TinySatoException("failed to send operations.", inner);
-            }
-            finally { Marshal.FreeCoTaskMem(raw); }
+            this.Send(flatten);
+            this.operations.Clear();
+            operation_start_index = 0;
+
             return flatten.Length;
         }
 
@@ -196,31 +224,66 @@
             operations.Add(Encoding.ASCII.GetBytes(OPERATION_Z + ETX));
 
             var flatten = operations.SelectMany(x => x).ToArray();
-            var raw = Marshal.AllocCoTaskMem(flatten.Length);
-            Marshal.Copy(flatten, 0, raw, flatten.Length);
-            int written = 0;
+            this.Send(flatten);
+            this.operations.Clear();
+            this.operations.Add(new byte[] { Convert.ToByte(STX) });
+            operation_start_index = this.operations.Count();
+
+            return flatten.Length;
+        }
+
+        void Send(byte[] raw)
+        {
+            if (printer != IntPtr.Zero) SendWin32(raw);
+            else if (client != null) SendTcp(raw);
+            else throw new NotImplementedException();
+        }
+
+        void SendWin32(byte[] raw)
+        {
+            var ptr = Marshal.AllocCoTaskMem(raw.Length);
+            Marshal.Copy(raw, 0, ptr, raw.Length);
+
             try
             {
                 if (!UnsafeNativeMethods.StartPagePrinter(printer))
                     throw new Win32Exception(Marshal.GetLastWin32Error());
-                if (!UnsafeNativeMethods.WritePrinter(printer, raw, flatten.Length, out written))
+                if (!UnsafeNativeMethods.WritePrinter(printer, ptr, raw.Length, out int written))
                     throw new Win32Exception(Marshal.GetLastWin32Error());
                 if (!UnsafeNativeMethods.EndPagePrinter(printer))
                     throw new Win32Exception(Marshal.GetLastWin32Error());
-                this.operations.Clear();
-                this.operations.Add(new byte[] { Convert.ToByte(STX) });
-                operation_start_index = this.operations.Count();
             }
-            catch (Win32Exception inner)
+            catch (Win32Exception e)
             {
-                throw new TinySatoException("failed to send operations.", inner);
+                throw new TinySatoException("failed to send operations for windows printer.", e);
             }
-            finally { Marshal.FreeCoTaskMem(raw); }
-            return flatten.Length;
+            finally
+            {
+                Marshal.FreeCoTaskMem(ptr);
+            }
+        }
+
+        void SendTcp(byte[] raw)
+        {
+            try
+            {
+                this.status = status.Refresh();
+                if (!status.OK)
+                    throw new TinySatoException($"Printer is failure. {status}");
+                client.Client.Send(raw);
+            }
+            catch (SocketException e)
+            {
+                throw new TinySatoException("failed to send operations by tcp.", e);
+            }
         }
 
         public void Close()
         {
+            if (client != null)
+            {
+                client.Close();
+            }
             if (printer != IntPtr.Zero && !UnsafeNativeMethods.EndDocPrinter(printer))
             {
                 var code = Marshal.GetLastWin32Error();
@@ -260,7 +323,7 @@
             if (disposing)
             {
                 this.Close();
-                this.operations.Clear();
+                if (client != null) client.Dispose();
             }
             disposed = true;
         }
